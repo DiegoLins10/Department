a849f85311f4ce9fbf9144736a0499c9da9f555e
diegolins10 <diegofernandeslins@gmail.com>
2021-08-16 18:45:37 -0300

    adicionando fotos no banco de dados

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Serialization;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 namespace WebAPI
 {
@@ -61,6 +63,16 @@ namespace WebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            /*
+             * Metodo para salvar as fotos
+             */
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+                RequestPath = "/Photos"
             });
         }
     }
