c619948dfa2776029a3cb5a33b91ce88c7ab98d3
diegolins10 <diegofernandeslins@gmail.com>
2021-08-16 17:34:25 -0300

    Controller Employee

---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebAPI.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        /*
+         * Injecao de dependencia
+         * pegando configuracoes do bd
+         */
+        private readonly IConfiguration _configuration;
+        /*
+         * Injecao de dependencia para colocar a foto no banco de dados
+         */
+        private readonly IWebHostEnvironment _env;
+
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        /*
+         * Metodo para pegar todas as informacoes da Table department
+         */
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select EmployeeId, EmployeeName, Department,
+                    convert(varchar(10),DateOfJoining,120) as DateOfJoining,
+                    PhotoFileName
+                    from dbo.Employee";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        /*
+         * Metodo para insert na table
+         */
+        [HttpPost]
+        public JsonResult Post(Employee emp)
+        {
+            string query = @"
+                    insert into dbo.Employee 
+                    (EmployeeName, Department, DateOfJoining, PhotoFilename)
+                    values 
+                    (
+                     '" + emp.EmployeeName + @"'
+                     ,'" + emp.Department + @"'
+                     ,'" + emp.DateOfJoining + @"'
+                     ,'" + emp.PhotoFileName + @"'
+                    )
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Sucessfully");
+        }
+
+        /*
+         * metodo para dar um 
+         * update no BD
+         */
+        [HttpPut]
+        public JsonResult Put(Employee emp)
+        {
+                string query = @"
+                    update dbo.Employee set 
+                    EmployeeName = '" + emp.EmployeeName + @"'
+                    ,Department = '" + emp.Department + @"'
+                    ,DateOfJoining = '" + emp.DateOfJoining + @"'                                 
+                    where EmployeeId = " + emp.EmployeeId + @";  
+                    ";
+                DataTable table = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+                return new JsonResult("Updated Sucessfully");
+            }
+            
+        /*
+         * metodo para deletar pelo Id
+         */
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from dbo.Employee
+                    where EmployeeId = " + id + @";  
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Sucessfully");
+        }
+
+        /*
+         * metodo para salvar a foto
+         * no bando de dados
+         */
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+                return new JsonResult(filename);
+            }
+            catch(Exception)
+            {
+                return new JsonResult("anonymous.png");
+            }
+        }
+
+        /*
+         * Metodo para pegar todos os departamentos
+         */
+        [Route("GetAllDepartmentNames")]
+        public JsonResult GetAllDepartmentNames()
+        {
+            string query = @"
+                    select DepartmentName from dbo.Department              
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
