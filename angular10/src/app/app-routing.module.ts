b18f0ed7481b1d55d286f040a69749da66cc3b2b
diegolins10 <diegofernandeslins@gmail.com>
2021-08-23 20:04:19 -0300

    criando os path da url

--- angular10/src/app/app-routing.module.ts
+++ angular10/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import{EmployeeComponent} from './employee/employee.component';
+import{DepartmentComponent} from './department/department.component';
+
+/* Path da url */
+const routes: Routes = [
+{path:'employee', component:EmployeeComponent},
+{path: 'department', component:DepartmentComponent},
+{path:'Employee', component:EmployeeComponent},
+{path: 'Department', component:DepartmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
