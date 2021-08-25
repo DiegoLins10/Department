5da15d2c76847d3921f48dab38b7f23c7c85c0e8
diegolins10 <diegofernandeslins@gmail.com>
2021-08-24 21:31:27 -0300

    criando metodos de click na interface

--- angular10/src/app/employee/show-emp/show-emp.component.ts
+++ angular10/src/app/employee/show-emp/show-emp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  {SharedService} from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-emp',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowEmpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  EmployeeList:any=[];
+
+  ModalTitle:string = "";
+  ActivateAddEditEmpComp:boolean=false;
+  emp:any;
 
   ngOnInit(): void {
+    this.refreshEmpList();
+  }
+
+  /* metodo para add um departamento com click */
+
+  addClick(){
+    this.emp={
+      EmployeeId:0,
+      EmployeeName:"",
+      Department:"",
+      DateOfJoining:"",
+      PhotoFileName:"anonymous.png"
+    }
+    this.ModalTitle="Add Employee";
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  editClick(item: any){
+    this.emp=item;
+    this.ModalTitle="Edit Employee";
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  deleteClick(item: any){
+    if(confirm('Are you sure??')){
+      this.service.deleteEmployee(item.EmployeeId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshEmpList();
+      })
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditEmpComp= false;
+    this.refreshEmpList();
+  }
+
+  /* atualizar a lista de departamento
+  usando o metodo subscribe com um assincrona operacao
+   */
+  refreshEmpList(){
+    this.service.getEmpList().subscribe(data=>{
+      this.EmployeeList = data;
+    })
   }
 
 }
+
